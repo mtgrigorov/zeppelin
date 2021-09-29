3a1b91ae4f923b7bfb755f5469fb4a22cebc5077
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2021-09-29 14:17:33 +0300

    ZEPPELIN-5543 Extract a script

---
+++ .cirrusci/scripts.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+lscpu
+
+java -version
+mvn -version
+
+echo "install application with some interpreter"
+mvn install -Pbuild-distr -DskipRat -DskipTests -pl zeppelin-server,zeppelin-web,spark-submit,spark/spark-dependencies,markdown,angular,shell -am -Phelium-dev -Pexamples -Phadoop3 --batch-mode --no-transfer-progress --fail-never
+
+echo "install application with some interpreter"
+mvn install -Pbuild-distr -DskipRat -DskipTests -pl zeppelin-server,zeppelin-web,spark-submit,spark/spark-dependencies,markdown,angular,shell -am -Phelium-dev -Pexamples -Phadoop3 --batch-mode --no-transfer-progress --fail-never
+
+echo "install and test plugins"
+mvn package -DskipRat -pl zeppelin-plugins,-org.apache.zeppelin:notebookrepo-mongo -amd -B
+
+echo "run tests with hadoop3"
+mvn verify -Pusing-packaged-distr -DskipRat -pl zeppelin-server,zeppelin-web,spark-submit,spark/spark-dependencies,markdown,angular,shell -am -Phelium-dev -Pexamples -Phadoop3 -Dtests.to.exclude=**/org/apache/zeppelin/spark/* -DfailIfNoTests=false
